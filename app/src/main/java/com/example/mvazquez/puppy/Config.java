5a36c1ff0ab984d15a8c85fb4a07d7ecd4ddee01
martinvs80 <martinvs80@hotmail.com>
2016-11-06 23:26:45 -0600

    Update Config.java

--- app/src/main/java/com/example/mvazquez/puppy/Config.java
+++ app/src/main/java/com/example/mvazquez/puppy/Config.java
@@ -4,6 +4,6 @@ package com.example.mvazquez.puppy;
  * Created by MVazquez on 06/11/2016.
  */
 public class Config {
-    public static final String EMAIL ="martinvs80@gmail.com";
-    public static final String PASSWORD ="Adivina";
+    public static final String EMAIL ="tucorreo@gmail.com";
+    public static final String PASSWORD ="tucontraseña";
 }
